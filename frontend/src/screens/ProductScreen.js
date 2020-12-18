0c2b374bf46bdfb057517f41016782e8ee528b76
anasali2193 <anasali2193@gmail.com>
2020-12-18 21:31:26 +0500

    2nd section

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,16 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { Row, Col, Image,ListGroup,Card,Button} from "react-bootstrap";
-import Rating from '../components/Rating';
-import products from '../products';
+import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import Rating from "../components/Rating";
+import axios from "axios";
+const ProductScreen = ({ match }) => {
+  // const product = products.find((product) => product._id === match.params.id);
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get("/api/products/" + match.params.id);
+      setProduct(data);
+    };
+    fetchProduct();
+  }, [match.params.id]);
+  return (
+    <>
+      <Link className="btn btn-light my-3" to="/">
+        Go Back
+      </Link>
+      <Row>
+        <Col md={5}>
+          <Image fluid src={product.image} alt={product.name} />
+        </Col>
+        <Col md={4}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+            <ListGroup.Item>Price : {product.price}</ListGroup.Item>
+            <ListGroup.Item>Description : {product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price :</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status :</Col>
+                  <Col>
+                    {product.countInStock > 0 ? `In Stock` : "Out of Stock"}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  disabled={product.countInStock === 0}
+                  type="button"
+                >
+                  Add to Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
+};
 
-const ProductScreen = ({match}) => {
-    const product = products.find((product) => product._id === match.params.id);
-    return (
-        <div>
-            {product.name}
-        </div>
-    )
-}
-
-export default ProductScreen
+export default ProductScreen;
